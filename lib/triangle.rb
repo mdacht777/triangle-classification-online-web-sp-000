542d7d5509a7c1facfd3413e0d5dde243be7b7d8
Martin Achtermann <mdacht777@gmail.com>
2020-07-05 20:21:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,14 @@
 class Triangle
   # write code here
   def initialize(a,b,c)
-  end
+    @a=a
+    @b=b 
+    @c=c
+    end
   class TriangleError < StandardError
     # triangle error code
   end
   def kind
+    puts ":equilateral" if @a=@b and @b=@c  
   end
 end
